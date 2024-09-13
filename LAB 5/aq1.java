e879460b21dfcb8c5e76c7f91c749041382eae48
SankalpBajpaiGit <154622097+SankalpBajpaiGit@users.noreply.github.com>
2024-09-14 03:06:31 +0530

    Update aq1.java

--- LAB 5/aq1.java
+++ LAB 5/aq1.java
@@ -1,1 +1,39 @@
+import java.util.*;
+abstract class calc{
+    abstract double area();
+}
+class square extends calc{
+    private double length;
+    public square(double length){
+        this.length = length;
+    }
+    public double area(){
+        return (length * length);
+    }
+}
+class triangle extends calc{
+    private double height , base;
+    public triangle(double height , double base){
+        this.height = height;
+        this.base = base;
+    }
+    public double area(){
+        return (0.5 * base * height);
+    }
+}
+class aq1{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the side of square");
+        double l;
+        l = sc.nextDouble();
+        System.out.println("Enter height and base of triangle");
+        double h = sc.nextDouble();
+        double b = sc.nextDouble();
+        square s = new square(l);
+        triangle t = new triangle(h , b);
+        System.out.println("Area of square is : "+s.area());
+        System.out.println("Area of triangle is : "+t.area());
 
+    }
+}
