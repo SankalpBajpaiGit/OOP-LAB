8a92987455719737abafead67bdf315076dcc344
SankalpBajpaiGit <154622097+SankalpBajpaiGit@users.noreply.github.com>
2024-09-14 02:55:46 +0530

    Update q2.c

--- LAB 5/q2.c
+++ LAB 5/q2.c
@@ -1,1 +1,39 @@
+import java.util.*;
+abstract class calc{
+    abstract double area();
+}
+class rectangle extends calc{
+    private double length , breath;
+    public rectangle(double length , double breath){
+        this.length = length;
+        this.breath = breath;
+    }
+    public double area(){
+        return (length * breath);
+    }
+}
+class circle extends calc{
+    private double radius;
+    public circle(double radius){
+        this.radius = radius;
+    }
+    public double area(){
+        return (3.14 * radius * radius);
+    }
+}
+class q2{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Length and bredth");
+        double l , b;
+        l = sc.nextDouble();
+        b = sc.nextDouble();
+        System.out.println("Enter radius");
+        double rad = sc.nextDouble();
+        rectangle r = new rectangle(l , b);
+        circle c = new circle(rad);
+        System.out.println("Area of rectangle is : "+r.area());
+        System.out.println("Area of circle is : "+c.area());
 
+    }
+}
